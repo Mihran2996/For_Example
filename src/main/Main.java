10204227c9fa9837a92ffe2752469c2e9099e9a1
Mihran_Maruqyan <Gyumri_2021>
2021-11-24 15:49:17 +0400

    initial commit

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("task 1 changed");
         System.out.println("task2 changed");
+        System.out.println("hello java!");
     }
 }
